9829716a37e2f6ed6f4e08b18263a4718f2cfb05
dzhou11 <dzhou11@hotmail.com>
2023-11-26 23:59:27 -0500

    empty project

---
+++ src/main/java/com/dzhou11/kebabcloud/KebabCloudApplication.java
@@ -0,0 +1,13 @@
+package com.dzhou11.kebabcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KebabCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KebabCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dzhou11/kebabcloud/KebabCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzhou11.kebabcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KebabCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
