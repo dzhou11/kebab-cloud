2bbe6d5af3e2a23402ed0584101bf34d820e1dbc
dzhou11 <dzhou11@hotmail.com>
2023-11-29 01:34:57 -0500

    fixed unit test 404 issue

--- src/test/java/com/dzhou11/kebabcloud/KebabCloudApplicationTests.java
+++ src/test/java/com/dzhou11/kebabcloud/KebabCloudApplicationTests.java
@@ -1,11 +1,31 @@
 package com.dzhou11.kebabcloud;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
-class KebabCloudApplicationTests {
+import static org.hamcrest.Matchers.containsString;
 
+@WebMvcTest(KebabCloudApplication.class)
+@Import(HomeController.class)
+public class KebabCloudApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void testHomePage() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("http://localhost:8080/"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.view().name("home"))
+				.andExpect(MockMvcResultMatchers.content().string(
+						containsString("Welcome to ...")
+				));
+	}
 	@Test
 	void contextLoads() {
 	}
